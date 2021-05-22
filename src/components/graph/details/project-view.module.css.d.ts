2aeed94ef2978f20e0b9ab97970b5cac50a3b3f3
Inver <jankowskipawelxd@gmail.com>
2021-05-22 20:39:30 +0200

    Modify Project View component

--- src/components/graph/details/project-view.module.css.d.ts
+++ src/components/graph/details/project-view.module.css.d.ts
@@ -1,1 +1,6 @@
 export const projectView: string
+export const header: string
+export const name: string
+export const date: string
+export const description: string
+export const repo: string
--- src/components/graph/details/project-view.tsx
+++ src/components/graph/details/project-view.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Project } from '../../../lib'
-import { projectView } from './project-view.module.css'
+import { projectView, header, name, date, description, repo } from './project-view.module.css'
 
 interface Props {
     project: Project
 }
 
-export const ProjectView: React.FC<Props> = ({ project: { name } }) => (
+export const ProjectView: React.FC<Props> = ({ project: { name: nameValue, creationDate, description: {description: descriptionValue}, openSource, repositoryUrl, previewUrl } }) => (
     <div className={projectView}>
-        {name}
+        <div className={header}>
+            <div className={name}>{nameValue}</div>
+            <div className={date}>{creationDate}</div>
+        </div>
+        {/* TODO show link anchor next to name and redirect to preview url*/}
+        <div className={description}>{descriptionValue}</div>
+        { openSource && <div className={repo}>{repositoryUrl}</div> }
     </div>
 )
