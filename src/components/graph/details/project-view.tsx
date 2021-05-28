4293a401dacc620e8d9809e4abcd0abecdc38378
Inver <jankowskipawelxd@gmail.com>
2021-05-28 16:26:07 +0200

    Change styles

--- src/components/graph/details/project-view.tsx
+++ src/components/graph/details/project-view.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Project } from '../../../lib'
 import { projectView, header, name, date, description, repo } from './project-view.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLockOpen, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 
 interface Props {
     project: Project
@@ -9,11 +11,13 @@ interface Props {
 export const ProjectView: React.FC<Props> = ({ project: { name: nameValue, creationDate, description: {description: descriptionValue}, openSource, repositoryUrl, previewUrl } }) => (
     <div className={projectView}>
         <div className={header}>
-            <div className={name}>{nameValue}</div>
+            <div className={name}>
+                { previewUrl ? <a href={previewUrl} target="_blank">{nameValue} <FontAwesomeIcon icon={faExternalLinkAlt}/></a> : <a>{nameValue}</a> }
+            </div>
             <div className={date}>{creationDate}</div>
         </div>
         {/* TODO show link anchor next to name and redirect to preview url*/}
         <div className={description}>{descriptionValue}</div>
-        { openSource && <div className={repo}>{repositoryUrl}</div> }
+        { openSource && <div className={repo}><a href={repositoryUrl} target="_blank"><FontAwesomeIcon icon={faExternalLinkAlt}/> Open-source</a></div> }
     </div>
 )
