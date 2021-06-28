7a582166929ac43f331df25b6b62e2684a1ebddb
Inver <jankowskipawelxd@gmail.com>
2021-06-28 09:42:24 +0200

    Add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { FunctionComponent } from 'react'
+
+import { footer } from './styles/footer.module.css'
+
+
+export const Footer: FunctionComponent = () => (
+    <footer className={footer}>
+        Copyright
+    </footer>
+)
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Media from 'react-media'
 
 import { Biography } from '../biography'
+import { Footer } from '../footer'
 import { SkillTree } from '../graph'
 import { Introduction } from '../introduction'
 import { Trait, Project } from '../../lib'
@@ -37,8 +38,8 @@ export const Layout: React.FC<Props> = ({ traits, projects, biographyChapters })
                 <Media queries={{ mobile: "(max-width: 799px)", desktop: "(min-width: 800px)" }}>
                     { matches => (
                         <>
-                            {matches.mobile && (<><Page>{introduction}</Page> <Page>{skilltree}</Page> <Page><Biography chapters={biographyChapters}/></Page></>)}
-                            {matches.desktop && (<><Page>{introduction} {skilltree}</Page> <Page><Biography chapters={biographyChapters}/></Page></>)}
+                            {matches.mobile && (<><Page>{introduction}</Page> <Page>{skilltree}</Page> <Page><Biography chapters={biographyChapters}/></Page> <Page><Footer /></Page></>)}
+                            {matches.desktop && (<><Page>{introduction} {skilltree}</Page> <Page><Biography chapters={biographyChapters}/></Page> <Page><Footer /></Page></>)}
                         </>
                     )}
                 </Media>
