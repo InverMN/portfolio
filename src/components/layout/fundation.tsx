4bdb81ec7ff47a4b23a599b31e7726c28db792b1
Inver <jankowskipawelxd@gmail.com>
2021-06-28 18:41:42 +0200

    Add page meta info

--- src/components/layout/fundation.tsx
+++ src/components/layout/fundation.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import { Helmet } from 'react-helmet'
+
 import { GlobalOverlayProvider } from '../../contexes/global-overlay-context'
 
 
 export const Fundation: React.FC = ({ children }) => (
     <>
+        <Helmet >
+            <meta charSet="utf-8" />
+            <title>Inver's Page</title>
+            <html lang="en" />
+        </Helmet>
         <GlobalOverlayProvider>
             {children}
         </GlobalOverlayProvider>
