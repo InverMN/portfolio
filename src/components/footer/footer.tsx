629e102f166a6901641bc4197709e717bab18330
Inver <jankowskipawelxd@gmail.com>
2021-06-28 10:04:54 +0200

    Create Copyright component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 import { FunctionComponent } from 'react'
 
+import { Copyright } from '.'
 import { footer } from './styles/footer.module.css'
 
 
 export const Footer: FunctionComponent = () => (
     <footer className={footer}>
-        Copyright
+        <Copyright />
     </footer>
 )
