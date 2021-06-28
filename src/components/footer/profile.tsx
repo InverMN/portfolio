770963cf6cb8ec786a3150e93b6615548adf74f7
Inver <jankowskipawelxd@gmail.com>
2021-06-28 19:05:30 +0200

    Style profile in footer

--- src/components/footer/profile.tsx
+++ src/components/footer/profile.tsx
@@ -4,11 +4,13 @@ import { FunctionComponent } from 'react'
 
 import { Avatar } from '../common'
 
+import { title } from './styles/common.module.css'
 import { profile } from './styles/profile.module.css'
 
 
 export const Profile: FunctionComponent = () => (
     <div className={profile}>
+        <div className={title}>PORTFOLIO</div>
         <Avatar />
         <div>Paweł Jankowski</div>
     </div>
