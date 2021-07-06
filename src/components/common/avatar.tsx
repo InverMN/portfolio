07644d34ee68d34660c898a564cbedcfd867b7a3
Inver <jankowskipawelxd@gmail.com>
2021-07-06 14:22:40 +0200

    Upload profile photo

--- src/components/common/avatar.tsx
+++ src/components/common/avatar.tsx
@@ -6,7 +6,7 @@ import { avatar, content } from './styles/avatar.module.css'
 export const Avatar: React.FC = () => (
     <div className={avatar}>
         <div className={content}>
-            <img src="https://png.pngtree.com/png-clipart/20190924/original/pngtree-user-vector-avatar-png-image_4830521.jpg" />
+            <img src="https://images.ctfassets.net/s0tmjwvqkpm2/3HYoqcEDeOl4QL1bMEStYq/9b13ed6eeb255ad7ca6a9c2a8e284366/avatar.png" />
         </div>
     </div>
 )
